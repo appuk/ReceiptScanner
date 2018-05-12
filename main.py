19706e81bcd2faf0856848cdd9cfe79b7203b18d
Apurva Kulkarni <bakshiapurva93@gmail.com>
2018-05-12 00:24:09 -0400

    adding all output in one file instead of different files

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import pytesseract
 import os
 
 
-for file in os.listdir("/home/apurva/Desktop/Receipts/recs"):
-    if file.endswith(".jpg"):
+var = raw_input("Please enter a relevant description for this batch of receipts: ")
+output_file = open (var,"w")
+for file in os.listdir("/home/apurva/Desktop/Receipts"):
+	if file.endswith(".jpg"):
 		im = Image.open(file)
 		text = pytesseract.image_to_string(im)
-		file = file[:-3]
-		textfile = file+"txt"
-		file = open (textfile,"w")
-		file.write(text.encode("utf-8")) 
-		file.close() 
+		textfile = file[:-3]
+		output_file.write("\n**************************************************************************")		
+		output_file.write("\nOutput for Receipt:\n")
+		output_file.write(textfile)
+		output_file.write("\n")		
+		output_file.write(text.encode("utf-8")) 
+		output_file.write("\n\n\n\n")	
+output_file.close() 
